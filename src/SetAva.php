c844ff3f7c4bb304e5221cbdd86fe1a46931fba8
Maxon <notmax2000@mail.ru>
2023-04-04 04:27:14 +0700

    v3

--- src/SetAva.php
+++ src/SetAva.php
@@ -7,6 +7,6 @@ trait SetAva
     public function setAva($img){
         $imgUniqueName = md5(time()).'.'.explode('/',$img['type'])[1];
         $this->ava = $imgUniqueName;
-        move_uploaded_file($img['tmp_name'], __DIR__ . '/../../public/Images/' . $imgUniqueName);
+        move_uploaded_file($img['tmp_name'], __DIR__ . '/../../../../public/Images/' . $imgUniqueName);
     }
 }
